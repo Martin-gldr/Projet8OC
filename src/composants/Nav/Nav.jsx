6034dfe6c80ab4cc262858ef0771a04db03e8d14
Martin-gldr <martin.glandier@gmail.com>
2025-07-01 11:37:27 +0200

    naviagation

---
+++ src/composants/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import '../Nav/Nav.css'
+export function Nav(){
+    return(
+
+        <div className="nav">
+            <a href='#1'>MES PROJETS</a>
+            <a href='#2'>MA FORMATION</a>
+            <a href='#3'>MES COMPETENCES</a>
+        </div>
+
+
+    )
+}
