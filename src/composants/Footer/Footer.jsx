e5d9839b0af497c51daaf813e2ccd0ab86951c29
Martin-gldr <martin.glandier@gmail.com>
2025-07-01 11:35:24 +0200

    header et footer

---
+++ src/composants/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import '../Footer/Footer.css'
+export function Footer() {
+    return(
+        <div className="footer">
+            <div className="footer-info"> 
+                <h3>Informations</h3>
+                <p> Nom : Glander</p>
+                <p> Prenom : Martin</p>
+
+                 
+            </div>
+            <div className="footer-contact">
+                <h3>Contact</h3>
+                <p>email: martin.glandier@gmail.com</p>
+
+            </div>
+        </div>
+    )
+}
