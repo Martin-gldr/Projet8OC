b778eda0058faec1d124156a7c48e3faea1b2823
Martin-gldr <martin.glandier@gmail.com>
2025-07-01 12:32:04 +0200

    modifs

--- src/composants/Header/Header.jsx
+++ src/composants/Header/Header.jsx
@@ -3,7 +3,7 @@ import "../Header/Header.css"
 export function Header() {
 return (
     <div className="banner">
-        <img fetchPriority='high' loading="eager" className="banner-img" src={banner} alt="background"/>
+        <img  className="banner-img" src={banner} alt="background"/>
         <h1 className="banner-title"> Développement WEB  </h1>
     </div>
 )
