98cf0ea8633f3e4936dab0362ea73552e956ae64
Martin-gldr <martin.glandier@gmail.com>
2025-07-01 11:36:31 +0200

    projets

---
+++ src/composants/Project/Project.jsx
@@ -0,0 +1,27 @@
+import '../Project/Project.css'
+export function Project (props){
+    const logo = props.logo
+    const desc = props.desc
+    const skillOne = props.skillOne
+    const skillTwo = props.skillTwo
+    const skillThree = props.skillThree
+    const skillThreeClass = props.skillThreeClass
+    return(
+        <div className='project'>
+            
+            <div className='project-title'> 
+                <img src={logo} alt={desc}/>
+                
+
+            </div> 
+            <p className='description'><span className='project-subtitle'>Description : </span> <br></br> {desc}</p>
+            <span className='project-subtitle'>Skills :</span> 
+            <div className='project-skill'>
+                <img src={skillOne} alt='skill one'/> 
+                <img src={skillTwo} alt ='skill two' />
+                <img src={skillThree} className={skillThreeClass} alt='skill three' />
+            </div>
+            
+        </div>
+    )
+}
---
+++ src/composants/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import '../Projects/Projects.css'
+import {Carousel} from '../Carousel/Carousel'
+export function Projects(){
+    return(
+        <div className='projects' id='1'>
+            <h2>Mes Projets</h2>
+            
+             <Carousel />
+                
+        
+        </div>
+    )
+}
