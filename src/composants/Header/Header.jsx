0177eb3af9f397c84e7caa0738572d8490a866de
Martin-gldr <martin.glandier@gmail.com>
2025-07-02 14:46:42 +0200

    ajout images carousel

--- src/composants/Header/Header.jsx
+++ src/composants/Header/Header.jsx
@@ -1,9 +1,9 @@
-import banner from "../../assets/banner.webp"
+
 import "../Header/Header.css"
 export function Header() {
 return (
     <div className="banner">
-        <img  className="banner-img" src={banner} alt="background"/>
+       
         <h1 className="banner-title"> Développement WEB  </h1>
     </div>
 )
--- src/composants/Project/Project.jsx
+++ src/composants/Project/Project.jsx
@@ -1,4 +1,7 @@
+import { useState} from 'react'
 import '../Project/Project.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faHandPointer as pointer} from '@fortawesome/free-solid-svg-icons'
 export function Project (props){
     const logo = props.logo
     const desc = props.desc
@@ -8,27 +11,55 @@ export function Project (props){
     const skillThreeClass = props.skillThreeClass
     const link = props.link
     const pb = props.pb
+    const pres = props.pres
+    let [displayI, setDisplayI]= useState('')
+    let [displayT, setDisplayT]= useState('hide')
+    
+        
+    
+    
+    
+
+    function change(){
+        if (displayI === ''){
+            setDisplayI('hide')
+            setDisplayT('')
+        }else if(displayT === ''){
+            setDisplayI('')
+            setDisplayT('hide')
+        }
+    }
+
     return(
-        <a className='project' href={link}>
-            
-            <div className='project-title'> 
-                <img src={logo} alt={desc}/>
-                <p className='description'><span className='project-subtitle'>Description : </span> <br></br> {desc} <br></br></p>
-                <p className='description'><span className='project-subtitle'>Problématiques : </span> <br></br> {pb}</p>
+        <div className='project'>
 
-            </div> 
-            
-            <div>
+            <div className='project-title'>
+                <div className='title-padding'>
+                    <img src={logo} alt={desc} />
+                    <div onClick={change} className={`text-presentation ${displayT}`}>
+                        <p className='description'><span className='project-subtitle'>Description : </span> <br></br> {desc} <br></br></p>
+                        <p className='description'><span className='project-subtitle'>Problématiques : </span> <br></br> {pb}</p>
+                         <a href={link}><span className='project-subtitle'>Lien :</span> {link} <br></br> </a>
+                    </div>
+                </div>
 
-                <span className='project-subtitle'>Skills :</span> 
+                <img onClick={change} id='presentation' className={displayI} src={pres} alt={desc} />
+                <i className={displayI}> <FontAwesomeIcon icon={pointer} /></i>
+
+            </div>
+
+            <div className='end-padding'>
+              
+
+                <span className='project-subtitle'>Skills :</span>
                 <div className='project-skill'>
-                    <img src={skillOne} alt='skill one'/> 
-                    <img src={skillTwo} alt ='skill two' />
+                    <img src={skillOne} alt='skill one' />
+                    <img src={skillTwo} alt='skill two' />
                     <img src={skillThree} className={skillThreeClass} alt='skill three' />
                 </div>
             </div>
-            
-            
-        </a>
+
+
+        </div>
     )
 }
